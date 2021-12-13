bfe5bd3fc5557598a424ea8144be62ec2dc05c69
진창훈 <jin.changhoon@mintech.kr>
2021-12-13 12:15:47 +0900

    Image, Card, State

--- 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
+++ 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
@@ -3,35 +3,71 @@ package kr.susemi99.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      LazyColumn(
-        Modifier
-          .background(Color.Green)
-          .fillMaxWidth(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
-      ) {
-        item { Text("header",
-          Modifier
-            .background(Color.Red)
-            .fillMaxWidth()) }
-        items(50) { index ->
-          Text("글씨 $index")
+      var isFavorite by rememberSaveable { mutableStateOf(false) }
+
+      ImageCard(
+        modifier = Modifier
+          .fillMaxWidth(0.5f)
+          .padding(16.dp),
+        isFavorite = isFavorite
+      ) { favorite -> isFavorite = favorite }
+    }
+  }
+}
+
+@Composable
+fun ImageCard(
+  modifier: Modifier = Modifier,
+  isFavorite: Boolean,
+  onTabFavorite: (Boolean) -> Unit
+) {
+  Card(
+    shape = RoundedCornerShape(8.dp),
+    elevation = 5.dp,
+    modifier = modifier
+  ) {
+    Box(Modifier.height(200.dp)) {
+      Image(
+        painter = painterResource(id = R.drawable.poster),
+        contentDescription = "포스터",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize()
+      )
+      Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
+        IconButton(onClick = {
+          onTabFavorite(!isFavorite)
+        }) {
+          Icon(
+            imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+            contentDescription = "favorite",
+            tint = Color.White
+          )
         }
-        item { Text("footer") }
       }
     }
   }
