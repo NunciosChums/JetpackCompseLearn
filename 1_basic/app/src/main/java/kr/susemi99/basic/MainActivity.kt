986778264b8d51675213a851526a79876cd3bbe4
진창훈 <jin.changhoon@mintech.kr>
2021-12-13 11:13:12 +0900

    preview, composable

--- 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
+++ 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
@@ -3,17 +3,11 @@ package kr.susemi99.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import kr.susemi99.basic.ui.theme.BasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,16 +17,30 @@ class MainActivity : ComponentActivity() {
       BasicTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Column(Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
-            .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Hello")
-            Text("World")
-          }
+          Greeting("Android")
         }
       }
     }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  BasicTheme {
+    Greeting("Android")
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+  BasicTheme {
+    Greeting("Android2")
   }
 }
