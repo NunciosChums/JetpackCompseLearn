00ea1787e2141ca2154511497d37745e6153b021
진창훈 <jin.changhoon@mintech.kr>
2021-12-13 11:53:56 +0900

    LazyColumn

--- 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
+++ 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
@@ -4,36 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import kr.susemi99.basic.ui.theme.BasicTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      BasicTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Box(Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(200.dp)) {
-            Text("Hello")
-            Box(Modifier
-              .fillMaxSize()
-              .padding(16.dp),
-              contentAlignment = Alignment.BottomEnd) {
-              Text("12345~~~~~~~~")
-            }
-          }
+      LazyColumn(
+        Modifier
+          .background(Color.Green)
+          .fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+      ) {
+        item { Text("header",
+          Modifier
+            .background(Color.Red)
+            .fillMaxWidth()) }
+        items(50) { index ->
+          Text("글씨 $index")
         }
+        item { Text("footer") }
       }
     }
   }
