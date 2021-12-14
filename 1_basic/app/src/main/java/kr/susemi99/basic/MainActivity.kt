ac3dfcc36d8e9fc4088afbc2ec930566f99b0238
진창훈 <jin.changhoon@mintech.kr>
2021-12-14 17:20:26 +0900

    state 심화

--- 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
+++ 1_basic/app/src/main/java/kr/susemi99/basic/MainActivity.kt
@@ -3,46 +3,44 @@ package kr.susemi99.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
-  private val viewModel by viewModels<MainViewModel>()
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column(
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-      ) {
-        Text(viewModel.data.value, fontSize = 30.sp)
-        Button(onClick = { viewModel.changeValue() }) {
-          Text("변경")
-        }
-      }
+      HomeScreen()
+    }
+  }
+}
+
+@Composable
+fun HomeScreen(viewModel: MainViewModel = viewModel()) {
+  val text3: State<String> = viewModel.data.observeAsState("hello world")
+  Column() {
+    Text(text3.value)
+    Button(onClick = { viewModel.changeValue(System.currentTimeMillis().toString()) }) {
+      Text("클릭")
     }
   }
 }
 
 class MainViewModel : ViewModel() {
-  private val _data = mutableStateOf("Hello")
-  val data: State<String> = _data
+  private val _data = MutableLiveData<String>()
+  val data: LiveData<String> = _data
 
-  fun changeValue() {
-    _data.value = "World"
+  fun changeValue(value: String) {
+    _data.value = value
   }
 }
