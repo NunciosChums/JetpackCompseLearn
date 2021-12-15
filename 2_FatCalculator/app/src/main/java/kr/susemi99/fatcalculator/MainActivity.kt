bbb27ac59d53afa9c97f231fad76a13f7621ae8a
진창훈 <jin.changhoon@mintech.kr>
2021-12-15 16:57:49 +0900

    결과 화면 작성

--- 2_FatCalculator/app/src/main/java/kr/susemi99/fatcalculator/MainActivity.kt
+++ 2_FatCalculator/app/src/main/java/kr/susemi99/fatcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.susemi99.fatcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -11,15 +12,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      HomeScreen()
+//      HomeScreen()
+      ResultScreen(bmi = 10.0)
     }
   }
 }
@@ -53,8 +58,13 @@ fun HomeScreen() {
   }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-  HomeScreen()
+fun ResultScreen(bmi: Double) {
+  Scaffold(topBar = { TopAppBar(title = { Text("비만도 계산기") }) }) {
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+      Text("과체중", fontSize = 30.sp)
+      Spacer(modifier = Modifier.height(50.dp))
+      Image(painter = painterResource(id = R.drawable.ic_face1), contentDescription = null, modifier = Modifier.size(100.dp), colorFilter = ColorFilter.tint(Color.Black))
+    }
+  }
 }
