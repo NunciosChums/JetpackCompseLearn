89cd1add6def2ae76b513ad7ad541776cb6baf5a
진창훈 <jin.changhoon@mintech.kr>
2021-12-20 17:53:32 +0900

    커스텀 웹브라우저 제작

--- WebBrowser/app/src/main/java/kr/susemi99/webbrowser/MainActivity.kt
+++ WebBrowser/app/src/main/java/kr/susemi99/webbrowser/MainActivity.kt
@@ -1,38 +1,115 @@
 package kr.susemi99.webbrowser
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import kr.susemi99.webbrowser.ui.theme.WebBrowserTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      WebBrowserTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
-      }
+      val vm = viewModel<MainViewModel>()
+      HomeScreen(vm)
     }
   }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun HomeScreen(vm: MainViewModel) {
+  val (url, setUrl) = rememberSaveable {
+    mutableStateOf("https://google.com")
+  }
+  val focusManager = LocalFocusManager.current
+  val scaffoldState = rememberScaffoldState()
+
+  Scaffold(topBar = {
+    TopAppBar(title = { Text("나만의 웹 브라우저") }, actions = {
+      IconButton(onClick = { vm.undo() }) {
+        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "이전 페이지", tint = Color.White)
+      }
+      IconButton(onClick = { vm.redo() }) {
+        Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "다음 페이지", tint = Color.White)
+      }
+    })
+  }, scaffoldState = scaffoldState) {
+    Column(Modifier.fillMaxWidth()) {
+      OutlinedTextField(
+        value = url,
+        onValueChange = setUrl,
+        label = { Text("https://") },
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(16.dp),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = {
+          vm.url.value = url
+          focusManager.clearFocus()
+        }),
+      )
+      Spacer(modifier = Modifier.height(8.dp))
+      MyWebView(vm, scaffoldState)
+    }
+  }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-  WebBrowserTheme {
-    Greeting("Android")
+fun MyWebView(vm: MainViewModel, scaffoldState: ScaffoldState) {
+  val webView = rememberWebView()
+
+  LaunchedEffect(Unit) {
+    vm.undoSharedFlow.collectLatest {
+      if (webView.canGoBack())
+        webView.goBack()
+      else
+        scaffoldState.snackbarHostState.showSnackbar("뒤로 갈 수 없습니다.")
+    }
+  }
+
+  LaunchedEffect(Unit) {
+    vm.redoSharedFlow.collectLatest {
+      if (webView.canGoForward())
+        webView.goForward()
+      else
+        scaffoldState.snackbarHostState.showSnackbar("앞으로 갈 수 없습니다.")
+    }
+  }
+
+  AndroidView(modifier = Modifier.fillMaxSize(),
+    factory = { webView },
+    update = { it.loadUrl(vm.url.value) })
+}
+
+@Composable
+fun rememberWebView(): WebView {
+  val context = LocalContext.current
+  return remember {
+    WebView(context).apply {
+      settings.javaScriptEnabled = true
+      webViewClient = WebViewClient()
+      loadUrl("https://google.com")
+    }
   }
 }
