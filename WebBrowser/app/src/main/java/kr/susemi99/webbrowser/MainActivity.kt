e1cf16befd04dba510eae764e8d91e32b99f10b7
진창훈 <jin.changhoon@mintech.kr>
2021-12-20 15:39:10 +0900

    웹 브라우저 앱 생성

---
+++ WebBrowser/app/src/main/java/kr/susemi99/webbrowser/MainActivity.kt
@@ -0,0 +1,38 @@
+package kr.susemi99.webbrowser
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import kr.susemi99.webbrowser.ui.theme.WebBrowserTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      WebBrowserTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  WebBrowserTheme {
+    Greeting("Android")
+  }
+}
