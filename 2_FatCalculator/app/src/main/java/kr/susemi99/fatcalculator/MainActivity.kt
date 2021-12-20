73758cf9bc89ae48540d48bbcdbc21ffb2f78c4a
진창훈 <jin.changhoon@mintech.kr>
2021-12-20 12:31:21 +0900

    비만도 계산기 작성

--- 2_FatCalculator/app/src/main/java/kr/susemi99/fatcalculator/MainActivity.kt
+++ 2_FatCalculator/app/src/main/java/kr/susemi99/fatcalculator/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -18,19 +22,37 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-//      HomeScreen()
-      ResultScreen(bmi = 10.0)
+      val viewModel = viewModel<BmiViewModel>()
+      val navController = rememberNavController()
+      val bmi = viewModel.bmi.value
+
+      NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+          HomeScreen { height, weight ->
+            viewModel.calculateBmi(height, weight)
+            navController.navigate("result")
+          }
+        }
+        composable("result") { ResultScreen(navController, bmi = bmi) }
+      }
     }
   }
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(onResultClicked: (Double, Double) -> Unit) {
   val (height, setHeight) = rememberSaveable { mutableStateOf("") }
   val (weight, setWeight) = rememberSaveable { mutableStateOf("") }
 
@@ -51,7 +73,11 @@ fun HomeScreen() {
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
       )
       Spacer(modifier = Modifier.height(8.dp))
-      Button(onClick = { }, Modifier.align(Alignment.End)) {
+      Button(onClick = {
+        if (height.isNotBlank() && weight.isNotBlank()) {
+          onResultClicked.invoke(height.toDouble(), weight.toDouble())
+        }
+      }, Modifier.align(Alignment.End)) {
         Text("결과")
       }
     }
@@ -59,12 +85,40 @@ fun HomeScreen() {
 }
 
 @Composable
-fun ResultScreen(bmi: Double) {
-  Scaffold(topBar = { TopAppBar(title = { Text("비만도 계산기") }) }) {
+fun ResultScreen(navController: NavController, bmi: Double) {
+  val text = when {
+    bmi >= 35 -> "고도비만"
+    bmi >= 30 -> "2단계 비만"
+    bmi >= 25 -> "1단계 비만"
+    bmi >= 23 -> "과체중"
+    bmi >= 18.5 -> "정상"
+    else -> "저체중"
+  }
+
+  val imageResId = when {
+    bmi >= 23 -> R.drawable.ic_face1
+    bmi >= 18.5 -> R.drawable.ic_face2
+    else -> R.drawable.ic_face3
+  }
+
+  Scaffold(topBar = {
+    TopAppBar(
+      title = { Text("비만도 계산기") },
+      navigationIcon = { Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "", modifier = Modifier.clickable { navController.popBackStack() }) })
+  }) {
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-      Text("과체중", fontSize = 30.sp)
+      Text(text, fontSize = 30.sp)
       Spacer(modifier = Modifier.height(50.dp))
-      Image(painter = painterResource(id = R.drawable.ic_face1), contentDescription = null, modifier = Modifier.size(100.dp), colorFilter = ColorFilter.tint(Color.Black))
+      Image(painter = painterResource(id = imageResId), contentDescription = null, modifier = Modifier.size(100.dp), colorFilter = ColorFilter.tint(Color.Black))
     }
+  }
+}
+
+class BmiViewModel : ViewModel() {
+  private val _bmi = mutableStateOf(0.0)
+  val bmi: State<Double> = _bmi
+
+  fun calculateBmi(height: Double, weight: Double) {
+    _bmi.value = weight / (height / 100.0).pow(2)
   }
 }
